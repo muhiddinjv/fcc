9164691b419dbb2fb781c7bc5c969f2d53a753db
Muhiddin <muhiddinjv@gmail.com>
2022-12-30 16:43:57 +0500

    feat: serve css with express.static

--- node/myApp.js
+++ node/myApp.js
@@ -1,11 +1,10 @@
-const { application } = require("express");
 let express = require("express");
 let app = express();
 
-console.log("Hello World");
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 module.exports = app;
